bcf4b6c2b1f754471d0c3363c5f3d7636cc5f013
Pushpakant Behera <pushpakantbehera@hotmail.com>
2020-12-31 20:06:27 +0545

    added profile image and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class GlobalDocument extends Document {
   metaDescription: string;
   metaTitle: string;
-  siteName: string;
+  siteUrl: string;
 
   constructor(props: any) {
     super(props);
-    this.metaDescription = 'Pushpakant Description'; //! change or remove description later
-    this.metaTitle = 'Pushpakant Behera'; //! change or remove title later
-    this.siteName = 'https://portfolio.pushpakant.vercel.app'; //! change or remove name later
+    this.metaDescription = 'This is my portfolio website.'; //! change or remove description later
+    this.metaTitle = 'Pushpakant Behera Portfolio'; //! change or remove title later
+    this.siteUrl = 'https://portfolio.pushpakant.vercel.app'; //! change or remove name later
   }
   render() {
     return (
@@ -18,31 +18,28 @@ export default class GlobalDocument extends Document {
           <meta charSet="utf-8" />
 
           <meta name="description" content={this.metaDescription} />
-          <meta
-            property="og:description"
-            content={this.metaDescription}
-            key="ogdesc"
-          />
-
-          <meta property="og:title" content={this.metaTitle} key="ogtitle" />
 
           {/* Twitter */}
           <meta name="twitter:card" content="summary" key="twcard" />
           {/* <meta name="twitter:creator" content={twitterHandle} key="twhandle" /> */}
 
           {/* Open Graph */}
-          <meta property="og:url" content={this.siteName} key="ogurl" />
-          <meta property="og:image" content="/profile.png" key="ogimage" />
-          <meta
-            property="og:site_name"
-            content={this.siteName}
-            key="ogsitename"
-          />
           <meta property="og:title" content={this.metaTitle} key="ogtitle" />
           <meta
             property="og:description"
             content={this.metaDescription}
             key="ogdesc"
+          />
+          <meta property="og:url" content={this.siteUrl} key="ogurl" />
+          <meta
+            property="og:image"
+            content={`${this.siteUrl}/profile.png`}
+            key="ogimage"
+          />
+          <meta
+            property="og:site_name"
+            content={this.metaTitle}
+            key="ogsitename"
           />
 
           {/* instructs search engine crawler to index the current page and follow any links on it*/}
