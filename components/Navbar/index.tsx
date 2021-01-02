f7e670c237a0c5c20fb69dd5e2f3c0b2685ea522
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-01-02 20:25:15 +0545

    made sidebar responsive

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
+  const [showMenu, setshowMenu] = useState(false);
+
   return (
     <nav className={styles.navContainer}>
       <div>
@@ -11,8 +13,39 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className={styles.listContainer}>
-        <ul className={styles.list}>
+      <div
+        className={styles.hamburgerMenuContainer}
+        onClick={() => {
+          setshowMenu(!showMenu);
+          document.body.style.overflow = 'hidden'; //  disabling scroll when menu is open
+        }}
+      >
+        <div className={showMenu ? styles.close : styles.hamburgerMenu}></div>
+      </div>
+
+      <div
+        className={styles.blurContainer}
+        style={
+          showMenu
+            ? { backdropFilter: 'blur(15px)' }
+            : { backdropFilter: 'blur(0px)' }
+        }
+      ></div>
+
+      <div
+        className={styles.listContainer}
+        style={
+          showMenu
+            ? { transform: 'translateX(0)' }
+            : { transform: 'translateX(100%)' }
+        }
+        onClick={() => setshowMenu(false)} //  closing navbar when the blurred container is clicked
+      >
+        <ul
+          className={styles.list}
+          onClick={(e) => e.stopPropagation()}
+          // stopping propagation of clickHandler to current and other children
+        >
           <li className={styles.listItem}>
             <Link href="/">
               <a>About</a>
