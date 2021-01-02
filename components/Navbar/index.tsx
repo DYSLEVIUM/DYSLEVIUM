d077ffc46e312faee002ddd3200f470e613dd669
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-01-02 22:43:43 +0545

    fixed menu animation

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.scss';
 
@@ -24,6 +24,11 @@ export default function Navbar() {
           setshowMenu(!showMenu);
           document.body.style.overflow = showMenu ? 'visible' : 'hidden'; //  disabling scroll when menu is open
         }}
+        style={
+          showMenu
+            ? { width: '1.5rem', height: '1.5rem' }
+            : { width: '1.5rem', height: '1rem' }
+        }
       >
         <div className={showMenu ? styles.close : styles.hamburgerMenu}></div>
       </div>
@@ -37,14 +42,7 @@ export default function Navbar() {
         }
       ></div>
 
-      <div
-        className={styles.listContainer}
-        style={showMenu ? { transform: 'translateX(0)' } : {}}
-        onClick={() => {
-          setshowMenu(false);
-          document.body.style.overflow = showMenu ? 'visible' : 'hidden'; //  disabling scroll when menu is open
-        }} //  closing navbar when the blurred container is clicked
-      >
+      <div className={styles.listContainer}>
         <ul
           className={styles.list}
           onClick={(e) => e.stopPropagation()}
@@ -61,6 +59,41 @@ export default function Navbar() {
             </Link>
           </li>
           <li className={styles.listItem}>
+            <Link href="/">
+              <a>Projects</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div
+        className={styles.listContainerAside}
+        style={
+          showMenu
+            ? { transform: 'translateX(0)' }
+            : { transform: 'translateX(100%)' }
+        }
+        onClick={() => {
+          setshowMenu(false);
+          document.body.style.overflow = showMenu ? 'visible' : 'hidden'; //  disabling scroll when menu is open
+        }} //  closing navbar when the blurred container is clicked
+      >
+        <ul
+          className={styles.listAside}
+          onClick={(e) => e.stopPropagation()}
+          // stopping propagation of clickHandler to current and other children
+        >
+          <li className={styles.listItemAside}>
+            <Link href="/">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className={styles.listItemAside}>
+            <Link href="/">
+              <a>Contact</a>
+            </Link>
+          </li>
+          <li className={styles.listItemAside}>
             <Link href="/">
               <a>Projects</a>
             </Link>
