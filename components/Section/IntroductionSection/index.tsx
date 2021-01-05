662df2055af710e4878c7f4b3379d995df7259cc
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-01-06 00:27:55 +0545

    added colors to introduction

--- components/Section/IntroductionSection/index.tsx
+++ components/Section/IntroductionSection/index.tsx
@@ -10,30 +10,26 @@ const IntroductionSection = () => {
       <motion.div
         initial={{ opacity: 0, translateY: 25 }}
         animate={{ opacity: 1, translateY: 0 }}
+        className={styles.introductionContainer}
       >
-        <img
-          src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif"
-          width="50px"
-          alt="Hello GIF"
-          style={{ filter: 'saturate(150%)', display: 'flex' }}
-        />
-
-        <span>
-          <h4 style={{ backgroundColor: '$primaryColor' }}>Hi, my name is</h4>
+        <span className={styles.salutation}>
+          <img
+            src="https://user-images.githubusercontent.com/1303154/88677602-1635ba80-d120-11ea-84d8-d263ba5fc3c0.gif"
+            alt="Hello GIF"
+          />
+          <h4>Hi, my name is</h4>
         </span>
-        <br />
-        <h1>Pushpakant Behera</h1>
-        <br />
-        <h4>I love to build things</h4>
-        <br />
-        <h5>
+
+        <h1>Pushpakant Behera.</h1>
+        <h2>I love to build things</h2>
+        <h3>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod nam in
           omnis cumque eum animi esse exercitationem temporibus eveniet
           perspiciatis. Similique animi atque impedit perspiciatis error maxime
           reiciendis recusandae vel. Quibusdam quasi culpa unde aperiam minima
           voluptas rem ipsum numquam tempore eligendi, vero ut facere
           voluptates. Voluptatum rem eaque eius!
-        </h5>
+        </h3>
       </motion.div>
     </div>
   );
