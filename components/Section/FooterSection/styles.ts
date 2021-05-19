e79c6187666cca8f8937489dca6313908f8588bb
Pushpakant Behrera <pushpakantbehera@hotmail.com>
2021-05-19 20:12:44 +0545

    fixed default export of bounce error

--- components/Section/FooterSection/styles.ts
+++ components/Section/FooterSection/styles.ts
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
-import { bounce } from '../../../styles/shared/animations/bounce';
+import bounce from '../../../styles/shared/animations/bounce';
 import {
   secondaryAccentColor,
   primaryAccentColor,
