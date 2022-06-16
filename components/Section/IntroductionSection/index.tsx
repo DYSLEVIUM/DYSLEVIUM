31f3e6d95f65cc1cc83c5162c4f3a3bd952bc54c
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-16 18:08:19 +0530

    Update index.tsx

--- components/Section/IntroductionSection/index.tsx
+++ components/Section/IntroductionSection/index.tsx
@@ -86,7 +86,7 @@ const IntroductionSection = () => {
           I love to solve problems and build things
         </CareerObjective>
         <ShortIntro variants={listItem}>
-          I am a junior at National Institute of Technology Jamshedpur, and have
+          I am a final year student at National Institute of Technology Jamshedpur, and have
           a avid passion for Software Engineering.
         </ShortIntro>
 
