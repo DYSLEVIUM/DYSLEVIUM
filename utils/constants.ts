3d61298d12edb7c4cd2bb4c653f2e5982f9077b9
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-17 10:40:10 +0530

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,7 +3,7 @@ export const githubUsername: string = 'DYSLEVIUM';
 export const githubProfile: string = `https://github.com/${githubUsername}`;
 export const linkedInProfile: string =
   'https://www.linkedin.com/in/pushpakant-behera-466ba4162';
-export const resumeUrl: string = "https://raw.githubusercontent.com/DYSLEVIUM/DYSLEVIUM/main/public/Pushpakant%20Behera%20Resume.pdf";
+export const resumeUrl: string = "https://raw.githubusercontent.com/DYSLEVIUM/DYSLEVIUM/main/public/resume.pdf";
 export const instagramProfile: string = 'https://www.instagram.com/dyslevium';
 export const redditUsername: string = 'Pushpakant';
 export const redditProfile: string = `https://www.reddit.com/user/${redditUsername}`;
