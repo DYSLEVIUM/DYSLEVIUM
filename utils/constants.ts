d92cdee3dbfb93a8975b7459e77c4318423f2bf7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-02-24 01:27:46 +0530

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,10 @@
-export const email: string = 'pushpakantbehera@hotmail.com';
+export const email: string = 'pushpakantbehera@outlook.com';
 export const githubUsername: string = 'DYSLEVIUM';
 export const githubProfile: string = `https://github.com/${githubUsername}`;
 export const linkedInProfile: string =
   'https://www.linkedin.com/in/pushpakantbehera/';
 export const resumeUrl: string = "https://raw.githubusercontent.com/DYSLEVIUM/DYSLEVIUM/main/public/Pushpakant%20Behera.pdf";
-export const instagramProfile: string = 'https://www.instagram.com/dyslevium';
+export const instagramProfile: string = 'https://www.instagram.com/pushpa.kant.behera/';
 export const redditUsername: string = 'Pushpakant';
 export const redditProfile: string = `https://www.reddit.com/user/${redditUsername}`;
 export const twitterUsername: string = 'DYSLEVIUM';
